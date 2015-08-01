b168bb3bb3fe51634139d13aba237e79989f2337
Jun Burden <junburden@gmail.com>
2015-08-01 16:11:46 +1200

    json used to populate location

--- jquery/main.js
+++ jquery/main.js
@@ -6,11 +6,14 @@ $(document).ready(function() {
 
 function addLocations() {
 	var tempList = ["Northland", "Auckland", "Waikato", "Bay of Plenty", "Gisborne", "Hawke's Bay", "Taranaki" , "Manawatu / Wanganui", "Wellington", "Nelson / Tasmin", "Malborough", "West Coast", "Otago", "Southland"];
-	var ld = $("#location-drop");
-	for(var value in tempList) {
-		var option = "<option>"+tempList[value]+"</option>";
-	  ld.append(option);
-	}
+	$.getJSON("https://api.trademe.co.nz/v1/Localities/Regions.json", function(data){
+		var ld = $("#location-drop");
+		for(pos in data) {
+		 	var value = data[pos].Name;
+		 	var option = "<option>"+value+"</option>";
+	    ld.append(option);
+		}
+	});
 }
 
 //Catagories
