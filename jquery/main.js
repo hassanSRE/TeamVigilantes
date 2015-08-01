3f11d89f98c40a7109188ea97b36c7bab17f89dd
Leon North <leonnorth@gmail.com>
2015-08-02 09:22:30 +1200

    tidy code

--- jquery/main.js
+++ jquery/main.js
@@ -33,14 +33,40 @@ function addCatagories() {
 }
 
 function callbackCatagory1(){
-	$('#category1-drop')
-            .change(function(e) {
-            	console.log($('#category1-drop').val());
-            })
-            .end()
+		$('#category1-drop')
+    .change(function(e) {
+    	//console.log($('#category1-drop').val());
+
+		$.getJSON("http://api.trademe.co.nz/v1/Categories.json", function(data){
+				var ld = $("#category1-drop");
+				// console.log(data);
+				data.Subcategories.forEach(function(value, key){
+					var valueName = value.Name;
+
+					if (valueName === $('#category1-drop').val()){
+						//console.log(value);
+						value.Subcategories.forEach(function(valuei, key){
+							console.log(valuei);
+						});
+					}
+
+				 	// var option = "<option>"+value+"</option>";
+			   //  ld.append(option);
+				})
+
+
+			});
+    	
+    })
+    .end()
+	// $('#category1-drop')
+ //    .change(function(e) {
+ //    	console.log($('#category1-drop').val());
+
+
+
+ //    })
+ //    .end()
 }
 
-function myFunction(){
-	console.log('hi');
-}
 
