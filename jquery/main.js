c99396c76c0f267afe8344c9496625df60c76cf0
Jun Burden <junburden@gmail.com>
2015-08-01 15:13:09 +1200

    dynamic dropdown with jquery

--- jquery/main.js
+++ jquery/main.js
@@ -1,10 +1,19 @@
+$(document).ready(
+	addLocations
+);
+
 //Locations
 var tempList = {1:"one", 2:"two", 3:"three"};
-$(document).ready(addLocations());
+
 
 function addLocations() {
-	alert("hi!");
-  $("#location-drop").append("<option>small</option>");
+	console.log("hi!");
+	var ourText = 'wat';
+	var option = "<option>"+ourText+"</option>";
+  var ld = $("#location-drop");
+  ld.append(option);
+  console.log(option);
+  console.log(ld);
 
 }
 
