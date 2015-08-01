905ffadd7575b9c441ba648d7bf75917417544fc
Leon North <leonnorth@gmail.com>
2015-08-02 11:20:18 +1200

    add self populating drops

--- jquery/main.js
+++ jquery/main.js
@@ -74,15 +74,85 @@ function updateDistricts(){
 }
 
 function catagory1Callback(){
-	$('#category1-drop').change(function(){updatecat();});
-	// $('#category2-drop').change(updateCat);
-	// $('#category3-drop').change(updateCat);
-}
-function updateCat1(){
-	c2 = $('#category2-drop');
+	$('#category1-drop').change(function(){updateCat2();});//update drop2 from 1
+	$('#category2-drop').change(function(){updateCat3();});//update drop3 from 2
+	$('#category3-drop').change(function(){updateCat4();});//update drop4 from 3
 }
 
-//selector item + object
+function updateCat2(){
+	//get the dropdown object to update
+	c2 = $('#category2-drop');
+	//empty whats already there
+	c2.empty();
+	//add new catagories
+	var currentCat = $('#category1-drop').val();
+	catagories.Subcategories.forEach(function(value, key){ //first layer
+		if (value.Name === currentCat){
+			value.Subcategories.forEach(function(value2, key){ //second layer
+				var name = value2.Name;
+				var option = "<option>"+name+"</option>";
+				c2.append(option);
+			});
+		}
+	});
+}
+
+function updateCat3(){
+	//get the dropdown object to update
+	c3 = $('#category3-drop');
+	//empty whats already there
+	c3.empty();
+	//add new catagories
+	var currentCat1 = $('#category1-drop').val();
+	var currentCat2 = $('#category2-drop').val();
+	catagories.Subcategories.forEach(function(value, key){ //first layer
+		if (value.Name === currentCat1){
+			value.Subcategories.forEach(function(value2, key){ //second layer
+				if (value2.Name === currentCat2){
+					value2.Subcategories.forEach(function(value3, key){ //third layer
+						var name = value3.Name;
+						var option = "<option>"+name+"</option>";
+						c3.append(option);
+					})
+				}
+			});
+		}
+	});
+}
+
+function updateCat4(){
+	//get the dropdown object to update
+	c4 = $('#category4-drop');
+	//empty whats already there
+	c4.empty();
+	//add new catagories
+	var currentCat1 = $('#category1-drop').val();
+	var currentCat2 = $('#category2-drop').val();
+	var currentCat3 = $('#category3-drop').val();
+
+	catagories.Subcategories.forEach(function(value, key){ //first layer
+		if (value.Name === currentCat1){
+			value.Subcategories.forEach(function(value2, key){ //second layer
+				if (value2.Name === currentCat2){
+					value2.Subcategories.forEach(function(value3, key){ //third layer
+						if (value3.Name === currentCat3){
+							
+							if(value3.Subcategories){//test that we aren't at the end!
+								value3.Subcategories.forEach(function(value4, key){ //fourth layer
+									var name = value4.Name;
+									var option = "<option>"+name+"</option>";
+									c4.append(option);
+								});
+							}else{
+								//DO SOMETHING HERE SO WE KNOW THAT IT IS THE END
+							}
+						}
+					})
+				}
+			});
+		}
+	});
+}
 
 
 
